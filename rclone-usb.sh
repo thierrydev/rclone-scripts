ddef74b6c41976c0f76fbee61e0327ae7975fead
Thierry Meliot <39303424+thierrydev@users.noreply.github.com>
2020-05-02 10:27:55 +0200

    Create rclone-usb.sh

---
+++ rclone-usb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# rclone current installer usb
+
+if pgrep "rclone";
+then
+    echo "Rclone is running. Please close all rclone instances before proceeding."
+    exit
+else
+    echo "Installing/Upgrading rclone stable..."
+    mkdir -p "$HOME"/.rclone-tmp
+    cd "$HOME"/.rclone-tmp || exit
+    wget https://downloads.rclone.org/rclone-current-linux-amd64.zip -O "$HOME"/.rclone-tmp/rclone.zip
+    unzip rclone.zip
+    cp "$HOME"/.rclone-tmp/rclone-v*/rclone "$HOME"/bin
+    cd "$HOME" || exit
+    rm -rf "$HOME"/.rclone-tmp
+    command -v rclone
+    rclone version
+fi
